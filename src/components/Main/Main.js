c956ffd38f7dbdfb1b60131d7ab488e439e4e9e7
Zaiko Eugene <euhene@linnovate.net>
2023-01-26 17:14:30 +0300

    correct all

---
+++ src/components/Main/Main.js
@@ -0,0 +1,18 @@
+
+
+import WelcomeSection from "../WelcomeSection/WelcomeSection";
+import ContactsSection from "../ContactsSection/ContactsSection";
+
+function Main() {
+
+  return ( <main>
+   <WelcomeSection/>
+
+
+
+    <ContactsSection/>
+  </main>
+  );
+}
+
+export default Main;
