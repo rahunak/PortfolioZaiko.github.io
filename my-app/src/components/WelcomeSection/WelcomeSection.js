09603ded87e632d4b5576d2852e4f3bcfc4f52eb
Zaiko Eugene <euhene@linnovate.net>
2023-01-26 09:23:54 +0300

    feat: added ContactSection and WelcomeSection

---
+++ my-app/src/components/WelcomeSection/WelcomeSection.js
@@ -0,0 +1,11 @@
+function WelcomeSection(){
+    return(
+        <section id="welcome-section" className="py-0 vh-100 text-center col">
+        <div className="mx-auto h-100 d-flex flex-column justify-content-center align-items-center">
+          <h1 className="fw-light display-1 fw-bold">Zaiko Eugeni</h1>
+          <p className="lead text-muted display-6">a web developer</p>
+        </div>
+      </section>
+    );
+}
+export default WelcomeSection;
