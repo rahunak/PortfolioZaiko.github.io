4a3d2ee803af9b8473af129cd7c17d330c0f951a
Zaiko Eugene <euhene@linnovate.net>
2023-01-27 14:41:55 +0300

    init: start migrate on typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,14 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Header from './components/header/header';
 
 function App() {
   return (
     <div className="App">
-      <Header />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
+          Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
