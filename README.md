dece09140d410c7f5069711f26b534233cd96556
Zaiko Eugene <euhene@linnovate.net>
2023-06-20 16:37:42 +0300

    feat + refactor: removed extra fields, also added new production files

--- README.md
+++
@@ -1,2 +0,0 @@
-# PortfolioZaiko.github.io
- My site is published at here https://rahunak.github.io/PortfolioZaiko.github.io/portfolio
--- src/index.js
+++
@@ -1,7 +0,0 @@
-// css
-import "./components/css/style.css";
-// scss
-import "./components/scss/main.scss";
-import "./components/scss/fonts.scss";
-// scripts
-import "./components/scripts/main";
